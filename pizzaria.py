188b9f05b583c40e096a09c1e9bb38b86c7f2569
João Fernandes da Silva <92832616+Fermatsilva@users.noreply.github.com>
2022-10-14 08:30:22 -0300

    Contrato

--- pizzaria.py
+++ pizzaria.py
@@ -1,1 +1,14 @@
+'''
+A função list_ingredientes receberá o tipo de ingrediente em forma de strign e o DataFrame com as opções. A opção será o índice e a coluna o seu valor. Ela printa para o usuário quais as opções e os respectivos valores.
+
+A função rem_ingredientes recebe os nomes dos ingredientes e o dicionário onde remover os ingredientes.
+
+A função add_ingredientes recebe os nomes dos ingredientes e o dicionário onde colocar os ingredientes, recebe também o dicionário com a lista de todos os ingredientes disponíveis e os seus preços.
+
+A função list_escolhas recebe o dicionários com os ingredientes escolhidos, printa os ingredientes e não retorna nada.
+
+A função calc_valor recebe a lista com os ingredientes escolhidos e retorna o valor final da pizza. 
+
+'''
+
 
